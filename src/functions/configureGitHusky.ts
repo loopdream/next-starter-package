20f18892a933242abd2a49e7abea7ea31da9f524
Dom Geargeoura <dom@loopdream.com>
2023-09-21 23:38:57 +0100

    husky install refactor 3

--- src/functions/configureGitHusky.ts
+++ src/functions/configureGitHusky.ts
@@ -21,21 +21,18 @@ const configureGitHusky = async ({
 
     if (packageManager.kind === PackageManagerKind.YARN) {
       await execa(`yarn`, [`dlx`, `husky-init`, `--yarn2`], { cwd: root });
+      await execa(`yarn`, [], { cwd: root });
     }
 
     if (packageManager.kind === PackageManagerKind.PNPM) {
       await execa(`pnpm`, [`dlx`, `husky-init`], { cwd: root });
+      await execa(`pnpm`, [`install`], { cwd: root });
     }
 
     if (packageManager.kind === PackageManagerKind.NPM) {
       await execa(`npx`, [`husky-init`], { cwd: root });
+      await execa(`npm`, [`install`], { cwd: root });
     }
-
-    await execa(
-      packageManager.kind,
-      packageManager.kind === PackageManagerKind.YARN ? [] : [`install`],
-      { cwd: root }
-    );
 
     addHuskySpinner.succeed();
   } catch (error) {
