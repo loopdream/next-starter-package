f5723dfb77eddc484590b370353a5b61fcfcc81c
Dominic Geargeoura <dominic.geargeoura@quantspark.com>
2023-10-04 17:37:34 +0100

    todo test

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `eslint ${filenames.join(' ')}`,
+    `eslint --fix ${filenames.join(' ')}`,
+    `tsc --noEmit`,
+    `leasot ${filenames.join(' ')} --exit-nicely`,
+  ],
+
+  '**/*.(md, yml, yaml, json)': (filenames) =>
+    `prettier --write ${filenames.join(' ')}`,
+};
