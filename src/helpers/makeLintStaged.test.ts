909de4519c9ddfc445b4dc6fae964412f05c6183
Dominic Geargeoura <dominic.geargeoura@quantspark.com>
2023-10-06 13:28:35 +0100

    tests

--- src/helpers/makeLintStaged.test.ts
+++ src/helpers/makeLintStaged.test.ts
@@ -2,145 +2,157 @@ import { PackageManagerKindEnum } from '../PackageManager.js';
 import makeLintStaged from './makeLintStaged.js';
 
 describe('makeLintStaged.config', () => {
-  it('should return a valid lint-staged configuration object', () => {
-    //test
-    const expectedConfig = {
-      '**/*.js?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'eslint .',
-        'eslint --fix .',
-        'jest --ci',
-      ],
-      '**/*.ts?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'eslint .',
-        'eslint --fix .',
-        'jest --ci',
-        'tsc --noEmit',
-      ],
-      '**/*.{md, yml, yaml, json}': [
-        'prettier --check .',
-        'prettier --write .',
-      ],
-      '**/*.{css}': ['prettier --check .', 'prettier --write .'],
-    };
+  describe('When eslint, jest, prettier and typescript options are true', () => {
+    it('should return a valid lint-staged configuration object', () => {
+      //test
+      const expectedConfig = {
+        '**/*.js?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'eslint .',
+          'eslint --fix .',
+          'jest --ci',
+        ],
+        '**/*.ts?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'eslint .',
+          'eslint --fix .',
+          'jest --ci',
+          'tsc --noEmit',
+        ],
+        '**/*.{md, yml, yaml, json}': [
+          'prettier --check .',
+          'prettier --write .',
+        ],
+        '**/*.{css}': ['prettier --check .', 'prettier --write .'],
+      };
 
-    const config = makeLintStaged.config({
-      eslint: true,
-      jest: true,
-      prettier: true,
-      typescript: true,
+      const config = makeLintStaged.config({
+        eslint: true,
+        jest: true,
+        prettier: true,
+        typescript: true,
+      });
+
+      expect(config).toEqual(expectedConfig);
     });
-
-    expect(config).toEqual(expectedConfig);
   });
 
-  it('should return a valid lint-staged configuration object without TypeScript', () => {
-    const expectedConfig = {
-      '**/*.js?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'eslint .',
-        'eslint --fix .',
-        'jest --ci',
-      ],
-      '**/*.{md, yml, yaml, json}': [
-        'prettier --check .',
-        'prettier --write .',
-      ],
-      '**/*.{css}': ['prettier --check .', 'prettier --write .'],
-    };
+  describe('When typescript is false and all other options are false', () => {
+    it('should return a valid lint-staged configuration object', () => {
+      const expectedConfig = {
+        '**/*.js?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'eslint .',
+          'eslint --fix .',
+          'jest --ci',
+        ],
+        '**/*.{md, yml, yaml, json}': [
+          'prettier --check .',
+          'prettier --write .',
+        ],
+        '**/*.{css}': ['prettier --check .', 'prettier --write .'],
+      };
 
-    const config = makeLintStaged.config({
-      eslint: true,
-      jest: true,
-      prettier: true,
-      typescript: false,
+      const config = makeLintStaged.config({
+        eslint: true,
+        jest: true,
+        prettier: true,
+        typescript: false,
+      });
+
+      expect(config).toEqual(expectedConfig);
     });
+  });
+  describe('When jest is false and all other options are false', () => {
+    it('should return a valid lint-staged configuration object', () => {
+      const expectedConfig = {
+        '**/*.js?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'eslint .',
+          'eslint --fix .',
+        ],
+        '**/*.ts?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'eslint .',
+          'eslint --fix .',
+          'tsc --noEmit',
+        ],
+        '**/*.{md, yml, yaml, json}': [
+          'prettier --check .',
+          'prettier --write .',
+        ],
+        '**/*.{css}': ['prettier --check .', 'prettier --write .'],
+      };
 
-    expect(config).toEqual(expectedConfig);
+      const config = makeLintStaged.config({
+        eslint: true,
+        jest: false,
+        prettier: true,
+        typescript: true,
+      });
+
+      expect(config).toEqual(expectedConfig);
+    });
+  });
+  describe('When prettier is false and all other options are false', () => {
+    it('should return a valid lint-staged configuration object without Prettier', () => {
+      const expectedConfig = {
+        '**/*.js?(x)': ['eslint .', 'eslint --fix .', 'jest --ci'],
+        '**/*.ts?(x)': [
+          'eslint .',
+          'eslint --fix .',
+          'jest --ci',
+          'tsc --noEmit',
+        ],
+      };
+
+      const config = makeLintStaged.config({
+        eslint: true,
+        jest: true,
+        prettier: false,
+        typescript: true,
+      });
+
+      expect(config).toEqual(expectedConfig);
+    });
   });
 
-  it('should return a valid lint-staged configuration object without Jest', () => {
-    const expectedConfig = {
-      '**/*.js?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'eslint .',
-        'eslint --fix .',
-      ],
-      '**/*.ts?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'eslint .',
-        'eslint --fix .',
-        'tsc --noEmit',
-      ],
-      '**/*.{md, yml, yaml, json}': [
-        'prettier --check .',
-        'prettier --write .',
-      ],
-      '**/*.{css}': ['prettier --check .', 'prettier --write .'],
-    };
+  describe('When eslint is false and all other options are false', () => {
+    it('should return a valid lint-staged configuration object without ESLint', () => {
+      const options = {
+        eslint: false,
+        jest: true,
+        prettier: true,
+        typescript: true,
+      };
+      const expectedConfig = {
+        '**/*.js?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'jest --ci',
+        ],
+        '**/*.ts?(x)': [
+          'prettier --check .',
+          'prettier --write .',
+          'jest --ci',
+          'tsc --noEmit',
+        ],
+        '**/*.{md, yml, yaml, json}': [
+          'prettier --check .',
+          'prettier --write .',
+        ],
+        '**/*.{css}': ['prettier --check .', 'prettier --write .'],
+      };
 
-    const config = makeLintStaged.config({
-      eslint: true,
-      jest: false,
-      prettier: true,
-      typescript: true,
+      const config = makeLintStaged.config(options);
+
+      expect(config).toEqual(expectedConfig);
     });
-
-    expect(config).toEqual(expectedConfig);
-  });
-
-  it('should return a valid lint-staged configuration object without Prettier', () => {
-    const expectedConfig = {
-      '**/*.js?(x)': ['eslint .', 'eslint --fix .', 'jest --ci'],
-      '**/*.ts?(x)': [
-        'eslint .',
-        'eslint --fix .',
-        'jest --ci',
-        'tsc --noEmit',
-      ],
-    };
-
-    const config = makeLintStaged.config({
-      eslint: true,
-      jest: true,
-      prettier: false,
-      typescript: true,
-    });
-
-    expect(config).toEqual(expectedConfig);
-  });
-
-  it('should return a valid lint-staged configuration object without ESLint', () => {
-    const options = {
-      eslint: false,
-      jest: true,
-      prettier: true,
-      typescript: true,
-    };
-    const expectedConfig = {
-      '**/*.js?(x)': ['prettier --check .', 'prettier --write .', 'jest --ci'],
-      '**/*.ts?(x)': [
-        'prettier --check .',
-        'prettier --write .',
-        'jest --ci',
-        'tsc --noEmit',
-      ],
-      '**/*.{md, yml, yaml, json}': [
-        'prettier --check .',
-        'prettier --write .',
-      ],
-      '**/*.{css}': ['prettier --check .', 'prettier --write .'],
-    };
-
-    const config = makeLintStaged.config(options);
-
-    expect(config).toEqual(expectedConfig);
   });
 });
 
