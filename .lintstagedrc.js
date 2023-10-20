ca55302e9bf5f3c5cda6ddf24895ee7575385ac2
Dom Geargeoura <dom@loopdream.com>
2023-10-20 12:45:35 +0100

    husky mods

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,6 @@
 export default {
   '**/*.[jt]s': (filenames) => [
-    `prettier --check ${filenames.join(' ')}`,
     `prettier --write ${filenames.join(' ')}`,
-    `eslint ${filenames.join(' ')}`,
     `eslint --fix ${filenames.join(' ')}`,
     `node --experimental-vm-modules --no-warnings node_modules/.bin/jest --ci --passWithNoTests --findRelatedTests ${filenames.join(
       ' '
@@ -11,7 +9,6 @@ export default {
   ],
 
   '**/*.(md, yml, yaml, json)': (filenames) => [
-    `prettier --check ${filenames.join(' ')}`,
     `prettier --write ${filenames.join(' ')}`,
   ],
 };
